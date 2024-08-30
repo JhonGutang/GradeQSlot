d0248d9c99ac3a0b15387adf231869e9aa20194c
Lindrey Empanto <73627452+lindrey499@users.noreply.github.com>
2024-08-30 20:48:17 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,3 +15,18 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+
+    
+    
+    
