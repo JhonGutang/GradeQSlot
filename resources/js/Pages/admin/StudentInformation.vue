f9c343cc8366ff7c40403d19c08b17a7144dd7ce
Jhon Barry Gutang <jbgutang771@gmail.com>
2024-10-14 21:43:27 +0800

    fixed student information not displaying after changing from inertia to axios

--- resources/js/Pages/admin/StudentInformation.vue
+++ resources/js/Pages/admin/StudentInformation.vue
@@ -2,35 +2,27 @@
 import AdminPageLayout from "../../Layouts/AdminPageLayout.vue";
 import Searchbar from "../../Components/Searchbar.vue";
 import { ref, computed } from "vue";
-import { router } from "@inertiajs/vue3";
 
-const props = defineProps({
-  error: Object,
-  students: Object,
-});
 
-// Reactive state for the search query
 const searchQuery = ref("");
 
-// Computed property to filter students based on the search query
-const filteredStudents = computed(() => {
-  if (!searchQuery.value) {
-    return props.students.data;
-  }
-  return props.students.data.filter(
-    (student) =>
-      student.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-      student.email.toLowerCase().includes(searchQuery.value.toLowerCase())
-  );
-});
 
-const handleSearch = (query) => {
-  searchQuery.value = query;
-};
+// const filteredStudents = computed(() => {
+//   if (!searchQuery.value) {
+//     return props.students.data;
+//   }
+//   return props.students.data.filter(
+//     (student) =>
+//       student.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+//       student.email.toLowerCase().includes(searchQuery.value.toLowerCase())
+//   );
+// });
 
-function showStudent(id) {
-  router.get('/admin/studentInfo/' + id);
-}
+// const handleSearch = (query) => {
+//   searchQuery.value = query;
+// };
+
+
 </script>
 
 <template>
@@ -49,34 +41,11 @@ function showStudent(id) {
         </thead>
         <tbody>
           <template v-for="student in filteredStudents" :key="student.id">
-            <tr 
-              @click="showStudent(student.id)" 
-              :class="{
-                'hover:bg-gray-300 cursor-pointer': true // Add hover and pointer styles
-              }"
-            >
-              <td>{{ student.id }}</td>
-              <td>{{ student.name }}</td>
-              <td>{{ student.gender }}</td>
-              <td>{{ student.email }}</td>
-            </tr>
+
           </template>
         </tbody>
       </v-table>
 
-      <div class="flex justify-center mt-4">
-        <Link
-          v-for="link in props.students.links"
-          :key="link.label"
-          :href="link.url"
-          v-html="link.label"
-          :class="{
-            'bg-indigo-500 text-white': link.active,
-            'border-gray-300 text-gray-700': !link.active,
-          }"
-          class="border p-2 mx-1 rounded"
-        />
-      </div>
     </v-container>
   </AdminPageLayout>
 </template>
